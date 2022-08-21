a9e02e6ffc3bd9adf6dde48dc34676fd9ff11d3e
luochao <luochao@joyy.com>
2022-08-21 21:55:57 +0800

    fix: add log type param validate

--- packages/lc-ee-cli/index.js
+++ packages/lc-ee-cli/index.js
@@ -2,6 +2,10 @@ export function eeCli() {
   console.log('This is eeCli')
 }
 
+const types = ['log', 'info', 'warn', 'error']
 export function log(message, type = 'log') {
+  if (!types.includes(type)) {
+    throw new Error('a invalid type')
+  }
   console[type](message)
 }
