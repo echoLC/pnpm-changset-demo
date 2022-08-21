1c38076417bb8c5ae708f764b0f79e4d0d3674d4
luochao <luochao@joyy.com>
2022-08-21 19:03:28 +0800

    feat: log 增加了 type

--- packages/lc-ee-cli/index.js
+++ packages/lc-ee-cli/index.js
@@ -2,6 +2,6 @@ export function eeCli() {
   console.log('This is eeCli')
 }
 
-export function log(message) {
-  console.log(message)
+export function log(message, type = 'log') {
+  console[type](message)
 }
